e208f372db12b1e416056e3614b3ffd8ef5e7316
José Quiñónez <joserobertoq@outlook.com>
2024-01-21 05:36:30 -0600

    Formulario Avanzado

---
+++ FundamentosBlazorServerForms/Modelos/SelectorCascada.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FundamentosBlazorServerForms.Modelos
+{
+	public class SelectorCascada
+	{
+		public int Selector1 { get; set; }
+
+        public int Selector2 { get; set; }
+
+    }
+}
+
---
+++ FundamentosBlazorServerForms/Modelos/TiposDatos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FundamentosBlazorServerForms.Modelos
+{
+	public class TiposDatos
+	{
+		public string PropString { get; set; }
+
+		public string PropPassword { get; set; }
+
+		public int PropEntero { get; set; }
+
+		public double PropDouble { get; set; }
+
+		public bool PropBool { get; set; }
+
+		public DateTime PropFecha { get; set; }
+
+		public string PropListaDesplegable { get; set; }
+	}
+}
+
